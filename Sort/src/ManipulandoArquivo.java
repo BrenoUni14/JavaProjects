4331f0b27a2724e30291381c4ebcbff2b10a4c04
BrenoUni14 <breno.carmo01@fatec.sp.gov.br>
2023-05-25 11:20:44 -0300

    String methods

--- Sort/src/ManipulandoArquivo.java
+++ Sort/src/ManipulandoArquivo.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package manipulandoarquivo;
+
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -13,25 +13,58 @@ import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author FATEC ZONA LESTE
- */
 public class ManipulandoArquivo {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args){
         try {
-            leituraArquivo();
-            escritaArquivo();
+            //leituraArquivo();
+            //escritaArquivo();
+            //metodoSubString();
+            //metodoSplit();
+            desafio();
         } catch (IOException ex) {
             Logger.getLogger(ManipulandoArquivo.class.getName()).log(Level.SEVERE, null, ex);
         }
         
         
         
+    }
+
+    private static void desafio() throws IOException{
+        final String ARQ_ENT = "C:\\Users\\FATEC ZONA LESTE\\Downloads\\entrada.txt";
+        FileReader fr = new FileReader(ARQ_ENT);//lê arquivos existentes
+        BufferedReader br = new BufferedReader(fr);//lê o conteudo
+
+        String linha = br.readLine();
+        while (linha != null) {
+            System.out.println(linha);
+            linha = br.readLine();
+        }
+    }
+
+    private static void metodoSplit() throws IOException{
+        /*
+        String nomeCompleto = "Breno Santos do Carmo";
+        String [] nomes = nomeCompleto.split(" ");
+        System.out.println(nomes[1]);
+        */
+        String dataAmericana = "2020/09/21";
+        String [] dataBR = dataAmericana.split("/");
+        for (int i = dataBR.length - 1 ; i >= 0; i--){
+            System.out.print(dataBR[i] + "/");
+        }
+
+    }
+
+    private static void metodoSubString() throws IOException{
+        String dataAmericana = "2020/09/21";
+        String dia = dataAmericana.substring(8,10);
+        String mes = dataAmericana.substring(5,7);
+        String ano = dataAmericana.substring(0,4);
+
+
+        String dataBR = dia+"/"+mes+"/"+ano ;
+        System.out.println(dataBR);
     }
 
     private static void leituraArquivo() throws IOException, FileNotFoundException {
